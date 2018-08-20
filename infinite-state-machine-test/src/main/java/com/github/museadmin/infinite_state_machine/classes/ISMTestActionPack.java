35257a71f9648a23a6f3145bda05f32e32d58108
Bradley Atkins <Bradley.Atkins@bjss.com>
2018-08-20 13:22:54 +0100

    Removed extra level from dir structure

--- infinite-state-machine-test/src/main/java/com/github/museadmin/infinite_state_machine/classes/ISMTestActionPack.java
+++
@@ -1,6 +0,0 @@
-package com.github.museadmin.infinite_state_machine.classes;
-
-import com.github.museadmin.infinite_state_machine.common.action.ActionPack;
-
-public class ISMTestActionPack extends ActionPack {
-}
