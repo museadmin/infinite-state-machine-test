531ecc40349b7d04bddb985f27b7f320ab5bc344
Bradley Atkins <Bradley.Atkins@bjss.com>
2018-08-20 13:23:35 +0100

    Removed extra level from dir structure

---
+++ src/main/java/com/github/museadmin/infinite_state_machine/classes/ActionAfterArchiveMessages.java
@@ -0,0 +1,25 @@
+package com.github.museadmin.infinite_state_machine.classes;
+
+import com.github.museadmin.infinite_state_machine.common.action.Action;
+import com.github.museadmin.infinite_state_machine.common.utils.ZipDirectory;
+
+import java.io.IOException;
+
+/**
+ * Archive the messaging directories and run log
+ */
+public class ActionAfterArchiveMessages extends Action {
+
+  public void execute() {
+    if(active()) {
+      String source = queryProperty("msg_root");
+      String target = "/tmp/msgs.zip";
+      try {
+        ZipDirectory.zipDirectory(source, target);
+      } catch (IOException e) {
+        LOGGER.error(String.format("Failed to create zip archive of messaging directory (%s);", target));
+      }
+      deactivate();
+    }
+  }
+}
